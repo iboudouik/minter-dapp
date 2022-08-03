9913cfac877505d22fc913b86f4e062481614451
iboudouik <boudouik@gmail.com>
2022-08-04 00:00:49 +0100

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const OWNER_ADDRESS = '0x43FdfbD88A03B8FD09763a0450Ced3E784a1f436';
 const TREASURY_ADDRESS = '0x43FdfbD88A03B8FD09763a0450Ced3E784a1f436';
 const MAX_SUPPLY = 3334; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 8; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-08-02T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
