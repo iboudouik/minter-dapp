cb44cac00b0608480533c6ada961ea12f5c89b8b
iboudouik <boudouik@gmail.com>
2022-08-06 01:34:11 +0100

    updated

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -10,13 +10,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   welcomeH2.innerText = welcome_h2;
   welcomeP.innerHTML = welcome_p;
 
-  const connectWalletActionBtn = document.getElementById(
-    "connectWalletActionBtn"
-  );
-  connectWalletActionBtn.onclick = () => {
-    document.getElementById("connectWallet").click();
-  };
-
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     checkChain();
