a1a12fdc546c9bb83adbc3cc453a490de96f55da
iboudouik <boudouik@gmail.com>
2022-08-06 21:13:01 +0100

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -37,7 +37,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "ttps://socratesgadfly.com", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://socratesgadfly.com", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
